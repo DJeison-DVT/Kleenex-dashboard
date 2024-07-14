94925cee30badab8886af7f4ae0bf054bf556299
Diego Villanueva <diegovt.arbt@gmail.com>
2024-07-14 01:29:46 +0000

    Add build args to github actions

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 const settings = {
-	apiUrl: import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:5173',
+	apiUrl: import.meta.env.VITE_REACT_APP_API_URL,
 	participationsURL: '/api/participations/',
 	tokenName: import.meta.env.VITE_REACT_APP_TOKEN_NAME,
 };
