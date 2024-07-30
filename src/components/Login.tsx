d238ee409e89030e6591c49aa20bf34596fc8b08
DJeison <diegovt.arbt@gmail.com>
2024-07-29 18:18:07 -0600

    landing login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,6 +5,7 @@ import {
 	Form,
 } from 'react-router-dom';
 import { Button } from './ui/button';
+import { Input } from './ui/input';
 
 export default function Login() {
 	let location = useLocation();
@@ -13,28 +14,42 @@ export default function Login() {
 
 	let navigation = useNavigation();
 	let isLoggingIn =
-		navigation.formData?.get('username') != null &&
+		navigation.formData?.get('username') != null ||
 		navigation.formData?.get('password') != null;
 
 	let actionData = useActionData() as { error: string } | undefined;
 
 	return (
-		<div>
-			<p>You must log in to view the page at {from}</p>
-
-			<Form method="post" replace>
+		<div className="h-screen w-screen flex justify-end items-center bg-black">
+			<div className=" flex flex-col flex-1 h-screen">
+				<img
+					src="landing-page.jpg"
+					alt="receipt taking"
+					className="object-cover h-full w-full"
+				/>
+			</div>
+			<Form
+				method="post"
+				replace
+				className="flex flex-col bg-dark text-white justify-center p-14 gap-2 w-[500px] h-screen"
+			>
+				<img src="demente-card.png" alt="demente" />
 				<input type="hidden" name="redirectTo" value={from} />
 				<label>
-					Usuario: <input name="username" />
+					Usuario: <Input className="text-black" name="username" />
 				</label>
 				<label>
-					Contraseña: <input name="password" />
+					Contraseña: <Input className="text-black" name="password" />
 				</label>
 				<Button type="submit" disabled={isLoggingIn}>
 					{isLoggingIn ? 'Logging in...' : 'Login'}
 				</Button>
 				{actionData && actionData.error ? (
-					<p style={{ color: 'red' }}>{actionData.error}</p>
+					<div className="flex">
+						<p className="w-full flex-0" style={{ color: 'red' }}>
+							{actionData.error}
+						</p>
+					</div>
 				) : null}
 			</Form>
 		</div>
