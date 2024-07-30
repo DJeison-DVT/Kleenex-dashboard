c648074572cf20e0648eeac557dd98e9def4a7de
DJeison <diegovt.arbt@gmail.com>
2024-07-30 11:40:56 -0600

    Sidebar users

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { NavLink, useFetcher, useRouteLoaderData } from 'react-router-dom';
-import { Gauge, ShoppingCart, Award, LogOut } from 'lucide-react';
+import { Gauge, ShoppingCart, Award, LogOut, Contact } from 'lucide-react';
 import { Button } from './ui/button';
+import React from 'react';
 
 function AuthStatus() {
 	let { user } = useRouteLoaderData('root') as { user: string | null };
@@ -22,7 +23,32 @@ function AuthStatus() {
 	);
 }
 
+interface NavigationTabProps {
+	to: string;
+	children: React.ReactNode;
+	end?: boolean;
+}
+const NavigationTab: React.FC<NavigationTabProps> = ({
+	to,
+	children,
+	end = false,
+}) => {
+	return (
+		<NavLink
+			to={to}
+			end={end}
+			className={({ isActive }) =>
+				(isActive ? 'bg-black/25' : '') + ' hover:bg-black/35'
+			}
+		>
+			<div className="flex gap-4">{children}</div>
+		</NavLink>
+	);
+};
+
 export default function Sidebar() {
+	let { role } = useRouteLoaderData('root') as { role: string | null };
+
 	return (
 		<div className="w-fit h-full flex flex-col">
 			<div className="bg-primary w-64 flex justify-center py-2">
@@ -34,40 +60,24 @@ export default function Sidebar() {
 			</div>
 			<div className="flex flex-col bg-dark flex-1">
 				<nav className="flex flex-col  text-primary flex-1 *:p-4">
-					<NavLink
-						to="/dashboard"
-						end
-						className={({ isActive }) =>
-							(isActive && 'bg-black/25') || 'hover:bg-black/35'
-						}
-					>
-						<div className="flex gap-4">
-							<Gauge />
-							Dashboard
-						</div>
-					</NavLink>
-					<NavLink
-						to="/dashboard/participations"
-						className={({ isActive }) =>
-							(isActive && 'bg-black/25') || 'hover:bg-black/35'
-						}
-					>
-						<div className="flex gap-4">
-							<ShoppingCart />
-							Participaciones
-						</div>
-					</NavLink>
-					<NavLink
-						to="/dashboard/prizes"
-						className={({ isActive }) =>
-							(isActive && 'bg-black/25') || 'hover:bg-black/35'
-						}
-					>
-						<div className="flex gap-4">
-							<Award />
-							Premios
-						</div>
-					</NavLink>
+					<NavigationTab to="/dashboard" end>
+						<Gauge />
+						Dashboard
+					</NavigationTab>
+					<NavigationTab to="/dashboard/participations">
+						<ShoppingCart />
+						Participaciones
+					</NavigationTab>
+					<NavigationTab to="/dashboard/prizes">
+						<Award />
+						Premios
+					</NavigationTab>
+					{role === 'admin' && (
+						<NavigationTab to="/dashboard/users">
+							<Contact />
+							Usuarios
+						</NavigationTab>
+					)}
 				</nav>
 				<AuthStatus />
 			</div>
