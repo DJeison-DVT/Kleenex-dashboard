f66d5f9938f26dfc55eb5a85e819ee2bcbd19a2d
DJeison <diegovt.arbt@gmail.com>
2024-07-31 23:56:10 -0600

    change participations url

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,7 @@ const apiBaseUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
 const settings = {
 	apiUrl: apiBaseUrl.startsWith('http') ? apiBaseUrl : `https://${apiBaseUrl}`,
-	participationsURL: '/api/participations/',
+	participationsURL: 'api/participations/',
 	tokenName: import.meta.env.VITE_REACT_APP_TOKEN_NAME,
 	bucketURL: import.meta.env.VITE_REACT_APP_BUCKET_URL,
 };
